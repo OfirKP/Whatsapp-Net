934d9c3b911cb6919975bcb01b72fed4ecaabcab
OfirK <magicofir11@gmail.com>
2019-09-23 22:02:18 +0300

    Add support for using multiple json data files and
    Unite data from multiple users by tracking and skipping already visited groups and keep adding edges

--- generate_graph.py
+++ generate_graph.py
@@ -9,47 +9,61 @@ import argparse
 
 DEFAULT_OUTPUT_PATH = 'graph.gexf'  # Path to save
 output_path = DEFAULT_OUTPUT_PATH
+contacts = None
+
+
+def insert_data_into_graph(data, graph, contacts=None):
+    groups = list(data.values())
+
+    if contacts:
+        for group in groups:
+            for contact in group["participants"][:]:
+                if contact in contacts:
+                    group["participants"].remove(contact)
+                    group["participants"].append(contacts[contact])
+
+    print("{} groups were found.".format(len(groups)))
+    for group_id, group in tqdm(data.items()):
+        if group_id in visited_groups:
+            # print(f"Already visited {group}!")
+            continue
+
+        visited_groups.add(group_id)
+        participants_cpy = group["participants"][:]
+        for participant in group["participants"]:
+            participants_cpy.remove(participant)
+            for neighbor in participants_cpy:
+                if graph.has_edge(participant, neighbor):
+                    graph[participant][neighbor]["weight"] += 1
+                else:
+                    graph.add_edge(participant, neighbor, weight=1)
+
 
 parser = argparse.ArgumentParser(description="Generate GEXF graph file from scraped data")
-parser.add_argument("data", type=str, help="path to data (.json) file")
+parser.add_argument("data", nargs="+", type=str, help="paths to data(.json)s files")
 parser.add_argument("-c", "--contacts", type=str, help="path to contacts (.json) file")
 parser.add_argument("-o", "--output", type=str, help="path to output GEXF file (default is {})"
                     .format(DEFAULT_OUTPUT_PATH))
 args = parser.parse_args()
 
-with open(args.data, mode='r', encoding='utf-8') as f:
-    data = json.load(f)
-
 if args.output:
     output_path = args.output
 
 if args.contacts:
-    with open(args.contacts, mode='r', encoding='utf-8') as f:
+    with open(args.contacts, 'r', encoding='utf-8') as f:
         contacts = json.load(f)
 
-    for group in data.values():
-        for contact in group["participants"][:]:
-            if contact in contacts:
-                group["participants"].remove(contact)
-                group["participants"].append(contacts[contact])
+visited_groups = set()
 
+print("Creating graph...")
 print()
 
 G = nx.Graph()
-groups = list(data.values())
-
-print("{} groups were found.".format(len(groups)))
-print("Creating graph...")
-for group in tqdm(groups):
-    participants_cpy = group["participants"][:]
-    for participant in group["participants"]:
-        participants_cpy.remove(participant)
-        for neighbor in participants_cpy:
-            if G.has_edge(participant, neighbor):
-                G[participant][neighbor]["weight"] += 1
-            else:
-                G.add_edge(participant, neighbor, weight=1)
-print()
+for path in args.data:
+    with open(path, mode='r', encoding='utf-8') as f:
+        groups_data = json.load(f)
+    insert_data_into_graph(groups_data, G, contacts=contacts)
+    print()
 
 print("Saving Graph...")
 nx.write_gexf(G, output_path)
