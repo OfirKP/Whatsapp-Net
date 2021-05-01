73d84d8cf58f04abb90de7e1241396423f7df78d
Ofir Krupik <ofirkrupik@gmail.com>
2021-05-01 22:11:32 +0300

    Significant improve in scraping time by making participants' fetching parallel

--- inject.js
+++ inject.js
@@ -1,18 +1,16 @@
 // Generate groups info to be downloaded
 async function generateGroupsInfo() {
-    const groups = WAPI.getAllGroups();
     let jsonObj = {};
-    for (group of groups)
-    {
+    const groups = WAPI.getAllGroups();
+    const getParticipantsIDs = groups.map((group) => WAPI.getGroupParticipantIDs(group.id));
+    const allParticipantsIDs = await Promise.all(getParticipantsIDs);
+    console.log(groups.length, allParticipantsIDs.length);
+    groups.forEach((group, index) => {
         let groupObj = {};
-        let participants = await WAPI.getGroupParticipantIDs(group.id);
-        
         groupObj["group_name"] = group.name;
-        groupObj["participants"] = participants.map(p => p.user);
-
+        groupObj["participants"] = allParticipantsIDs[index].map(p => p.user);
         jsonObj[group.id._serialized] = groupObj;
-    }
-
+    });
     return jsonObj;
 }
 
@@ -21,8 +19,7 @@ async function getContactsInfo() {
     const contacts = WAPI.getMyContacts();
     let jsonObj = {};
 
-    for (const contact of contacts)
-    {
+    for (const contact of contacts) {
         jsonObj[contact.id.user] = contact.name;
     }
 
@@ -32,15 +29,14 @@ async function getContactsInfo() {
 // Download given content as file with filename
 function download(content, fileName, contentType) {
     var a = document.createElement("a");
-    var file = new Blob([content], {type: contentType});
+    var file = new Blob([content], { type: contentType });
     a.href = URL.createObjectURL(file);
     a.download = fileName;
     a.click();
 }
 
 // The scraper's main function
-async function run()
-{
+async function run() {
     console.log("Downloading contacts...");
     const contacts = await getContactsInfo();
     console.log(contacts);
@@ -50,7 +46,7 @@ async function run()
     const data = await generateGroupsInfo();
     console.log(data);
     download(JSON.stringify(data, null, 4), 'data.json', 'application/json');
-    
+
 }
 
 run().then(() => console.log("Downloaded all info!"));
